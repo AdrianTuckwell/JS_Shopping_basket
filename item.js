28335b0b53615968f7f1aca0cbf02c4839ddbf3d
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-30 07:18:44 +0000

    updated item

--- item.js
+++ item.js
@@ -3,30 +3,5 @@ var item = {
   price: 2.49
 };
 
-var jaffaCakes = {
-  name: "Jaffa Cakes",
-  price: 2.49
-};
-
-var milk = {
-  name: "Milk",
-  price: 0.69
-};
-
-var chicken = {
-  name: "Chicken",
-  price: 5.49
-};
-
-var eggs = {
-  name: "eggs",
-  price: 10.00
-};
-
-
-module.exports = jaffaCakes;
-module.exports = milk;
-module.exports = chicken;
-module.exports = eggs;
 module.exports = item;
 
